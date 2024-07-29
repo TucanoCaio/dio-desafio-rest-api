f0291c43008d460e80a0516300f6e5010b282780
tucano <caio.tucano@outlook.com>
2024-07-29 20:44:59 -0300

    doc: primeiro commit

---
+++ src/main/java/me/dio/dio_desafio_rest_api/DioDesafioRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioRestApiApplication.class, args);
+	}
+
+}
