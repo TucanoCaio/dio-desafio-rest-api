7164c8cadb778e10153158bc17cbb8991c656901
tucano <caio.tucano@outlook.com>
2024-07-29 21:20:08 -0300

    commit

--- src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
+++ src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
@@ -1,10 +1,10 @@
-package me.dio.dio_desafio_rest_api;
+package me.dio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DioDesafioRestApiApplicationTests {
+class SantanderDevWeek2023ApplicationTests {
 
 	@Test
 	void contextLoads() {
